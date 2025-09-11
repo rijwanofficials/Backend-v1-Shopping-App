b33b61a27313982bb9879b57b532d045f3e20616
Rijwan Husain <husainrijwan2001@gmail.com>
2025-09-11 14:56:57 +0530

    update cors configuration: refactor allowed origins handling for improved flexibility

--- app.js
+++ app.js
@@ -8,9 +8,17 @@ const cookieParser = require('cookie-parser')
 
 const PORT = process.env.PORT || 3900;
 
+
+const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true
 }));
 
